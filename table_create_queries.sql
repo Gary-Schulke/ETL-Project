274c1fb87890ba77ece72307a487040054fe0838
gary-schulke <gary.schulke@outlook.com>
2019-11-23 14:01:18 -0800

    Added db creation

--- table_create_queries.sql
+++ table_create_queries.sql
@@ -1,3 +1,16 @@
+-- Database: ETL_Project
+
+-- DROP DATABASE "ETL_Project";
+
+CREATE DATABASE "ETL_Project"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
 -- Create tables for raw data to be loaded into
 CREATE TABLE cities (
 city_id INT PRIMARY KEY,
